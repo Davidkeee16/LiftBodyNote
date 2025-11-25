e295693423d2de1b225a4a6aed1bd216da2e4734
David <davidkeee16@gmail.com>
2025-11-25 12:32:14 +0200

    feat: add HomeViewModel

---
+++ LiftBodyNote/MVVM/ViewModel/HomeViewModel.swift
@@ -0,0 +1,70 @@
+import SwiftUI
+import SwiftData
+
+// MARK: - ExerciseListMode
+
+enum ExerciseListAddMode {
+    case custom
+    case system
+}
+
+// MARK: - ExerciseListViewModel
+
+final class HomeViewModel: ObservableObject {
+    // MARK: - Published state
+    
+    @Published var isAddOptionsPresented: Bool = false
+    @Published var addMode: ExerciseListAddMode? = nil
+    @Published var errorMessage: String? = nil
+    
+    // MARK: - Dependencies
+    
+    private(set) var repository: ExerciseRepository!
+    
+    init() {}
+    
+    func configure(context: ModelContext) {
+        if repository == nil {
+            self.repository = SwiftDataExerciseRepository(context: context)
+        }
+    }
+    
+    
+    // MARK: - Intents
+    
+    func didTapAddButton() {
+        isAddOptionsPresented = true
+    }
+    func selectCustom() {
+        addMode = .custom
+        isAddOptionsPresented = false
+    }
+    func selectSystem() {
+        addMode = .system
+        isAddOptionsPresented = false
+    }
+    func addCustomExercise(
+        name: String,
+        muscleGroup: String,
+        notes: String?
+    ) {
+        do {
+            try repository.addCustomExercise(name: name,
+                                             muscleGroup: muscleGroup,
+                                             notes: notes
+            )
+            errorMessage = nil
+        } catch {
+            errorMessage = "Failed to save custom exercise"
+        }
+    }
+    func addSystemExrcise(from preset: Exercise) {
+        do {
+            try repository.addSystemExercise(from: preset)
+            errorMessage = nil
+        } catch {
+            errorMessage = "Failed to save system exercise"
+        }
+    }
+}
+
